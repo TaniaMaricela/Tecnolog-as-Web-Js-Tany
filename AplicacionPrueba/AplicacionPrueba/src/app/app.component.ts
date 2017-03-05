c82b6d996c6ec8c8aff6973039dc107fb16ec09c
Tania Guamushig <tania.guamushig@epn.edu.ec>
2017-03-05 17:39:37 -0500

    Commit

--- AplicacionPrueba/AplicacionPrueba/src/app/app.component.ts
+++ AplicacionPrueba/AplicacionPrueba/src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+
+// export class AppComponent {
+//   title = 'app works!';
+
+// CTRL A +  -  CTRL + ALT + L dar formato
+
+export class AppComponent implements OnInit {
+  title: string = "Hola Amigos";
+  nombre: string = "";
+  apellido: string = "";
+  colorH4 = "red";
+  tamanoH4 = "52px";
+  classes = "btn btn-block btn-success";
+
+  constructor() {
+    this.apellido = "Guamushig";
+    this.nombre = "Tania";
+    console.log("Inicio el constructor")
+  }
+
+  ngOnInit() {
+    this.apellido = "Aimacaña";
+    this.nombre = "Maricela";
+    console.log("On Init")
+  }
+
+  nombreCompleto(): string {
+    return `${this.nombre} ${this.apellido}`
+  }
+
+  hizoClick() {
+    console.log("Hizo Click");
+    console.log()
+  }
+
+  hizoFocus() {
+    console.log("Hizo focus");
+  }
 }
--- AplicacionPrueba/AplicacionPrueba/src/app/app.module.ts
+++ AplicacionPrueba/AplicacionPrueba/src/app/app.module.ts
@@ -5,6 +5,11 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+// DECORATOR
+//@Decorator({asd:"asd"})
+//@Primary({})
+//@Politecnica({asda:"asdasd"})
+
 @NgModule({
   declarations: [
     AppComponent
